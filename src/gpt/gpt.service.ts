76615a5ed903066ae6faddf49f50f1e7cd92f081
Juan Sebastian Martinez <sebastianmartinez778@gmail.com>
2025-02-18 23:01:15 -0500

    Audio

--- src/gpt/gpt.service.ts
+++ src/gpt/gpt.service.ts
@@ -1,6 +1,9 @@
-import { Injectable } from '@nestjs/common';
-import { orthographyCheckUseCase, prosConsDicusserStreamUseCase, prosConsDicusserUseCase, translateUseCase } from './use-cases';
-import { OrthographyDto, ProsConsDiscusserDto, TranslateDto } from './dtos';
+import * as path from "path";
+import * as fs from "fs";
+
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { orthographyCheckUseCase, prosConsDicusserStreamUseCase, prosConsDicusserUseCase, textToAudioUseCase, translateUseCase } from './use-cases';
+import { OrthographyDto, ProsConsDiscusserDto, TextToAudioDto, TranslateDto } from './dtos';
 import OpenAI from 'openai';
 
 @Injectable()
@@ -26,5 +29,18 @@ export class GptService {
 
     async translate({ prompt, lang }: TranslateDto) {
         return await translateUseCase( this.openAi, { prompt, lang });
+    }
+
+    async textToAudio({ prompt, voice }: TextToAudioDto) {
+        return await textToAudioUseCase( this.openAi, { prompt, voice });
+    }
+
+    async textToAudioGetter( fileId: string ) {
+        const filePath = path.resolve( __dirname, '../../generated/audios', `${fileId}.mp3`);
+        const wasFound = fs.existsSync(filePath);
+
+        if( !wasFound ) throw new NotFoundException(`File ${ fileId } not found`);
+
+        return filePath;
     }
 }
